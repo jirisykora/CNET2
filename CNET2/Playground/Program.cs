8fb1d58311fe17c81479efe797a11a997264a3eb
jirisykora <sykorj1@gmail.com>
2021-11-22 10:41:10 +0100

    1. ukol

--- CNET2/Playground/Program.cs
+++ CNET2/Playground/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+
+var ustrings = strings.Select(x => x.ToUpper());
