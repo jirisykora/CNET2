640b74e3b4d546df3d140bf903c4c7a7ba5f5c28
jirisykora <sykorj1@gmail.com>
2021-11-22 11:14:12 +0100

    3. uloha reseni

--- CNET2/Playground/Program.cs
+++ CNET2/Playground/Program.cs
@@ -4,14 +4,23 @@ Console.WriteLine("Hello, World!");
 var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-// 1 preved strings na uppercase
+// 1 strings: preved strings na uppercase
 var ustrings = strings.Select(x => x.ToUpper());
 
 PrintList(ustrings.ToList());
 
- // 2 zjistit v poli suda cisla
+ // 2 numbers: zjistit v poli suda cisla
 
 bool isEvenNumbers = numbers.All(x => x % 2 == 0);
+
+Console.WriteLine($"jsou vsechna cisla suda: {isEvenNumbers}");
+
+// 3 -vypiste cisla v poli numbers jako slova
+
+foreach (var number in numbers)
+{
+    Console.WriteLine($"{strings[number]}");
+}
 
 static void PrintList(List<string> list)
 {
