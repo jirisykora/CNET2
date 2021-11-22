2e7519f898eee72a5772542d3dae46fe150f7352
jirisykora <sykorj1@gmail.com>
2021-11-22 15:40:01 +0100

    7. uloha / reading files

--- CNET2/Playground/Program.cs
+++ CNET2/Playground/Program.cs
@@ -7,55 +7,75 @@ Console.WriteLine("Hello, World!");
 var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-// 1 strings: preved strings na uppercase
-var ustrings = strings.Select(x => x.ToUpper());
+//// 1 strings: preved strings na uppercase
+//var ustrings = strings.Select(x => x.ToUpper());
 
-PrintList(ustrings.ToList());
+//PrintList(ustrings.ToList());
 
- // 2 numbers: zjistit v poli suda cisla
+// // 2 numbers: zjistit v poli suda cisla
 
-bool isEvenNumbers = numbers.All(x => x % 2 == 0);
+//bool isEvenNumbers = numbers.All(x => x % 2 == 0);
 
-Console.WriteLine($"jsou vsechna cisla suda: {isEvenNumbers}");
+//Console.WriteLine($"jsou vsechna cisla suda: {isEvenNumbers}");
 
-// 3 -vypiste cisla v poli numbers jako slova
+//// 3 -vypiste cisla v poli numbers jako slova
 
-var result = numbers.Select(x => strings[x]);
-PrintList(result.ToList());
-//foreach (var number in numbers)
-//{
-//    Console.WriteLine($"{strings[number]}");
-//}
+//var result = numbers.Select(x => strings[x]);
+//PrintList(result.ToList());
+////foreach (var number in numbers)
+////{
+////    Console.WriteLine($"{strings[number]}");
+////}
 
 
-// 4 - zjistěte kolik obsahují všechna
-// slova v poli "strings" dohromady písmen
+//// 4 - zjistěte kolik obsahují všechna
+//// slova v poli "strings" dohromady písmen
 
-var count = strings.Select(x=> x.Length).Sum();
-Console.WriteLine($"Soucet vsech pismen v poli strings: {count}");
+//var count = strings.Select(x=> x.Length).Sum();
+//Console.WriteLine($"Soucet vsech pismen v poli strings: {count}");
 
-// 5 - 
+//// 5 - 
 
-var strUL = strings.Select(x => new UpperLowerString(x))
-                   .Select(objX => $"upper:{objX.UpperCase} lower:{objX.LowerCase}");
+//var strUL = strings.Select(x => new UpperLowerString(x))
+//                   .Select(objX => $"upper:{objX.UpperCase} lower:{objX.LowerCase}");
 
-PrintList(strUL.ToList()); 
- // 5 - pomoci tuplu
+//PrintList(strUL.ToList()); 
+// // 5 - pomoci tuplu
 
-var res = strings.Select(slovo => (slovo.ToUpper(), slovo.ToLower()));
+//var res = strings.Select(slovo => (slovo.ToUpper(), slovo.ToLower()));
 
-PrintItems<(string, string)>(res);
+//PrintItems<(string, string)>(res);
 
-// 6 -frekvence vyskytu jednotlivych pismen ve vsech polozkach pole strings
+//// 6 -frekvence vyskytu jednotlivych pismen ve vsech polozkach pole strings
 
-var OneString = string.Join("", strings);  // spojim slova do jednoho retezce
-var res2 = OneString                                    // pracuji se stringem jako s kolekci znaku
-                    .GroupBy(x => x)                    // seskupuji podle pismenek (char v lkolekci string)
-                    .Select(g => (g.Key, g.Count()))   // udelam tuple obsahujici klic a pocet vyskytu
-                    .OrderByDescending(x => x.Item2)
-                    .ThenBy(x => x.Key)
-                    ;    
-PrintItems<(char, int)>(res2);
+//var OneString = string.Join("", strings);  // spojim slova do jednoho retezce
+//var res2 = OneString                                   // pracuji se stringem jako s kolekci znaku
+//                    .GroupBy(x => x)                   // seskupuji podle pismenek (char v lkolekci string)
+//                    .Select(g => (g.Key, g.Count()))   // udelam tuple obsahujici klic a pocet vyskytu
+//                    .OrderByDescending(x => x.Item2)
+//                    .ThenBy(x => x.Key)
+//                    ;    
+//PrintItems<(char, int)>(res2);
+
+// 7. Dictionary
+
+var bookdir = @"D:\Source\Repos\CNET2\CNET2\Playground\Books";
+
+foreach (var file in GetFilesFromDir(bookdir))
+{
+    var dict = TextTools.WordAnalyze(file);
+    var top10 = TextTools.GetTopTen(10, dict);
+
+    var fi = new FileInfo(file);
+
+    Console.WriteLine("KNIHA: " + fi.Name);
+    PrintList(top10.Select(x => $"{x.Key} : {x.Value}").ToList());
+    Console.WriteLine();
+}
+
+
+Console.WriteLine();
+
 
 // PRINT LIST
 static void PrintList(List<string> list)
@@ -72,4 +92,30 @@ static void PrintItems<T>(IEnumerable<T> items)
     {
         Console.WriteLine(item);
     }
+}
+
+static Dictionary<char, int> CharFreq(string input)
+{
+    var tuples = input                                 // pracuji se stringem jako s kolekci znaku
+                    .GroupBy(x => x)                   // seskupuji podle pismenek (char v lkolekci string)
+                    .Select(g => (g.Key, g.Count()))   // udelam tuple obsahujici klic a pocet vyskytu
+                    .OrderByDescending(x => x.Item2)
+                    .ThenBy(x => x.Key)
+                    ;
+
+    Dictionary<char, int> dict = new Dictionary<char, int>();
+
+    foreach (var item in tuples)
+    {
+        dict.Add(item.Key, item.Item2);
+    }
+
+    return dict;
+
+
+}
+
+static IEnumerable<string> GetFilesFromDir(string dir)
+{
+    return Directory.EnumerateFiles(dir);
 }
